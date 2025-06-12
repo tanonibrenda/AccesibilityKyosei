d268f39112f20a673b3aad2ca77512b19f40f2c6
tanonibrenda <brendayohenatanoni@gmail.com>
2025-06-12 11:41:28 -0300

    verSiSubeDist

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  esbuild: {
+    loader: 'jsx',
+    include: /\.jsx?$/,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      },
+    },
+  },
+});
