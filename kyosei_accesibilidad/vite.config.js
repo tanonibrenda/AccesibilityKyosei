9313b5f67efad8fe48116346e0112fd4a06ec261
tanonibrenda <brendayohenatanoni@gmail.com>
2025-06-12 10:40:41 -0300

    pruebaJueves10AM

--- kyosei_accesibilidad/vite.config.js
+++ kyosei_accesibilidad/vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  // base: './',
   plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
 })
